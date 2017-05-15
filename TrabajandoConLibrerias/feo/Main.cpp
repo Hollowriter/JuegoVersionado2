538a164b2ee4419763d6cefbb9a161c083263630
48 <48>
2017-05-15 16:16:58 -0300

    Libreria implementada, pero las ventanas te tiran errores

---
+++ TrabajandoConLibrerias/feo/Main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"..\SFML-2.4.2\include\SFML\Window.hpp"
+using namespace std;
+int main() {
+	sf::Window window(sf::VideoMode(800, 600), "CosaFea");
+	while (window.isOpen()){
+		sf::Event event;
+		while (window.pollEvent(event)){
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+	}
+	return 0;
+}
