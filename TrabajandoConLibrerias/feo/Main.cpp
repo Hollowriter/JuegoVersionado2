e411314f3cfb2b0bc009453bf3ce43832a842b54
817 <817>
2017-05-16 12:42:07 -0300

    Se mueve constantemente hacia la derecha

--- TrabajandoConLibrerias/feo/Main.cpp
+++ TrabajandoConLibrerias/feo/Main.cpp
@@ -3,6 +3,7 @@
 #include"SFML\Graphics.hpp"
 using namespace std;
 int main() {
+	sf::Time contando = sf::seconds(0.01f);
 	sf::RenderWindow window(sf::VideoMode(800, 600), "CosaFea");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Red);
@@ -12,6 +13,7 @@ int main() {
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+		shape.move(contando.asSeconds(), 0);
 		window.clear(sf::Color::White);
 		window.draw(shape);
 		window.display();
