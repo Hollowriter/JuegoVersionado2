994e472cc70dbb60736e3ece298dd56c1c7932bd
817 <817>
2017-05-16 11:49:18 -0300

    Ventana abierta

--- TrabajandoConLibrerias/feo/Main.cpp
+++ TrabajandoConLibrerias/feo/Main.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 int main() {
 	sf::Window window(sf::VideoMode(800, 600), "CosaFea");
-	/*while (window.isOpen()){
+	while (window.isOpen()){
 		sf::Event event;
 		while (window.pollEvent(event)){
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-	}*/
+	}
 	return 0;
 }
