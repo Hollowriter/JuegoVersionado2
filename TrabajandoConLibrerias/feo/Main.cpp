1d84a37ed95467476d9b2dfd1e164aa5b23db6af
817 <817>
2017-05-16 12:25:02 -0300

    Circulo rojo y fondo blanco

--- TrabajandoConLibrerias/feo/Main.cpp
+++ TrabajandoConLibrerias/feo/Main.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
-#include"..\SFML-2.4.2\include\SFML\Window.hpp"
+#include"SFML\Window.hpp"
+#include"SFML\Graphics.hpp"
 using namespace std;
 int main() {
-	sf::Window window(sf::VideoMode(800, 600), "CosaFea");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "CosaFea");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Red);
 	while (window.isOpen()){
 		sf::Event event;
 		while (window.pollEvent(event)){
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+		window.clear(sf::Color::White);
+		window.draw(shape);
+		window.display();
 	}
 	return 0;
 }
