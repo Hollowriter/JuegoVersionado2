01594b77a88db0a6ae2c9da6dfb1904818816235
817 <817>
2017-05-16 13:01:44 -0300

    cuadrado de otro color (Movimiento en proceso)

--- TrabajandoConLibrerias/feo/Main.cpp
+++ TrabajandoConLibrerias/feo/Main.cpp
@@ -6,7 +6,11 @@ int main() {
 	sf::Time contando = sf::seconds(0.01f);
 	sf::RenderWindow window(sf::VideoMode(800, 600), "CosaFea");
 	sf::CircleShape shape(100.f);
+	sf::RectangleShape thyRectangle(sf::Vector2f(100, 100));
+	int square_x = 0;
+	int square_y = 0;
 	shape.setFillColor(sf::Color::Red);
+	thyRectangle.setFillColor(sf::Color::Blue);
 	while (window.isOpen()){
 		sf::Event event;
 		while (window.pollEvent(event)){
@@ -14,8 +18,25 @@ int main() {
 				window.close();
 		}
 		shape.move(contando.asSeconds(), 0);
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+			square_x -= contando.asSeconds();
+			thyRectangle.move(square_x, square_y);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+			square_x += contando.asSeconds();
+			thyRectangle.move(square_x, square_y);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+			square_y -= contando.asSeconds();
+			thyRectangle.move(square_x, square_y);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+			square_y += contando.asSeconds();
+			thyRectangle.move(square_x, square_y);
+		}
 		window.clear(sf::Color::White);
 		window.draw(shape);
+		window.draw(thyRectangle);
 		window.display();
 	}
 	return 0;
