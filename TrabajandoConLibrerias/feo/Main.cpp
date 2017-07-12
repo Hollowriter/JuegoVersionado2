e17d18ec0b4606761e43dd1f911bbc645c26b546
121 <121>
2017-07-12 16:34:12 -0300

    carpeta assets creada

--- TrabajandoConLibrerias/feo/Main.cpp
+++ TrabajandoConLibrerias/feo/Main.cpp
@@ -1,18 +1,8 @@
 #include"Game.h"
 using namespace std;
-
-#if DEBUG
 int main() {
 	Game* juego = new Game();
 	juego->Menu();
 	delete juego;
 	return 0;
 }
-#else
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char*, int nShowCmd) {
-	Game* juego = new Game();
-	juego->Menu();
-	delete juego;
-	return 0;
-}
-#endif
